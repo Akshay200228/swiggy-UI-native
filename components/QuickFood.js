cc54eec30eaa050cbbc61bc7171242f2865a0953
Akshay200228 <sankpla2812@gmail.com>
2023-07-31 15:43:50 +0530

    Complete Quick Food Component

--- components/QuickFood.js
+++ components/QuickFood.js
@@ -1,6 +1,7 @@
 import { ImageBackground, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import quickFood from '../data/quickfood'
+import { MaterialIcons } from '@expo/vector-icons';
 
 const QuickFood = () => {
     const data = quickFood;
@@ -17,14 +18,25 @@ const QuickFood = () => {
                         style={{ margin: 10 }}
                     >
                         <ImageBackground
-                            imageStyle={{borderRadius: 6}}
+                            imageStyle={{ borderRadius: 6 }}
                             source={{ uri: item.image }}
                             style={{ aspectRatio: 5 / 6, height: 170 }}
                         >
-                            <Text style={{position: "absolute", bottom: 10, left: 10, fontSize: 27, fontWeight: "900", color: "white"}}>
+                            <Text style={{ position: "absolute", bottom: 10, left: 10, fontSize: 27, fontWeight: "900", color: "white" }}>
                                 {item.offer} OFF
                             </Text>
                         </ImageBackground>
+                        <Text style={{ marginTop: 10, fontSize: 15, fontWeight: "500" }}>
+                            {item.name}
+                        </Text>
+                        <View 
+                            style={{flexDirection:"row", alignItems:"center", marginTop:4}}
+                        >
+                            <MaterialIcons name="stars" size={24} color="green" />
+                            <Text style={{marginLeft:3, fontSize:15, fontWeight:"400"}}>{item.rating}</Text>
+                            <Text style={{marginLeft:3}}>•</Text>
+                            <Text style={{marginLeft:3, fontSize:15, fontWeight:"400"}}>{item.time}mins</Text>
+                        </View>
                     </Pressable>
                 ))}
             </ScrollView>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,22 @@
-import { StyleSheet, SafeAreaView, View, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, View, TextInput, ScrollView, Pressable, Text } from 'react-native';
 import React from 'react';
+// Icons
+import { Ionicons } from '@expo/vector-icons';
+
+// Data
+import hotels from '../data/hotels'
+
 import { AntDesign } from '@expo/vector-icons';
 import Carousel from '../components/Carousel';
 import FoodTypes from '../components/FoodTypes';
 import QuickFood from '../components/QuickFood';
+import MenuItem from '../components/MenuItem';
 
 const HomeScreen = () => {
+  const data = hotels;
+
   return (
-    <ScrollView style={{marginTop: 50}}>
+    <ScrollView style={{ marginTop: 50 }}>
       {/* Searvh Bar */}
       <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", borderWidth: 1, margin: 10, padding: 10, borderColor: "#C0C0C0", borderRadius: 7 }}>
         <TextInput style={{ fontSize: 17 }} placeholder='Search for Restaurant item or more.' />
@@ -23,6 +32,64 @@ const HomeScreen = () => {
       {/* Quick Food Component */}
       <QuickFood />
 
+      {/* Filter Buttons Component */}
+
+      <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-around" }}>
+        {/* Btn 1 */}
+        <Pressable
+          style={{
+            marginHorizontal: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            borderWidth: 1,
+            borderColor: "#D0D0D0",
+            padding: 10,
+            borderRadius: 20,
+            width: 120,
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ marginRight: 6 }}>Filter</Text>
+          <Ionicons name="filter" size={20} color="black" />
+        </Pressable>
+
+        {/* Btn 2 */}
+        <Pressable
+          style={{
+            marginHorizontal: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            borderWidth: 1,
+            borderColor: "#D0D0D0",
+            padding: 10,
+            borderRadius: 20,
+            width: 120,
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ marginRight: 6 }}>Sort By Rating</Text>
+        </Pressable>
+
+        {/* Btn 3 */}
+        <Pressable
+          style={{
+            marginHorizontal: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            borderWidth: 1,
+            borderColor: "#D0D0D0",
+            padding: 10,
+            borderRadius: 20,
+            width: 120,
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ marginRight: 6 }}>Sort By Price</Text>
+        </Pressable>
+      </View>
+      {data.map((item, index) => (
+        <MenuItem key={index} item={item} />
+      ))}
     </ScrollView>
   );
 };
